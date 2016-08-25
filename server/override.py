cf45b906ffdf78d95b0f510b5cbd1b7726f20e1c
Rico Sennrich <rico.sennrich@gmx.ch>
2016-08-25 10:09:19 +0100

    fix encoding issue with override mechanism

--- server/override.py
+++ server/override.py
@@ -1,17 +1,18 @@
 from override_patterns import patterns
 
-
 def override(sentences, translation_memory):
     forced_translation = {}
 
     for i, sentence in enumerate(sentences):
+        # use translation memory for exact matches
         if translation_memory and sentence in translation_memory:
             forced_translation[i] = translation_memory[sentence]
             continue
 
+        # copy sentences that match a Regex pattern
         for pattern in patterns:
             if pattern.search(sentence):
-                forced_translation[i] = sentence
+                forced_translation[i] = sentence.encode('UTF-8')
                 break
 
     return forced_translation
